2c751f67f34aa8ea7b6b6d42b22c1b2b63bfbfad
Kobe <kobenorcross@gmail.com>
2021-07-11 12:51:20 -0700

    Creating various classes for the data structure

--- pokemon.js
+++ pokemon.js
@@ -1,8 +1,9 @@
+`
 import * as data from 'pokemon.json';
 const pokemon = data;
 console.log(JSON.parse(pokemon))
 
-`
+
 let pokemon;    
 fetch('pokemon.json').then(
         function(u){ return u.json();}
@@ -23,3 +24,51 @@ const pokemon = getData('pokemon.json');
 
 console.log({ pokemon })
 `
+
+
+const file = '/Users/kobenorcross/Documents/GitHub/Pokemon-Database'
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+Class Pokemon {
+    // NUMBER,CODE,SERIAL,NAME,TYPE1,TYPE2,COLOR,ABILITY1,ABILITY2,ABILITY HIDDEN,GENERATION,LEGENDARY,MEGA_EVOLUTION,HEIGHT,WEIGHT,HP,ATK,DEF,SP_ATK,SP_DEF,SPD,TOTAL
+    
+}
+
+Class Node {
+    constructor (data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+}
+
+Class Tree {
+    constructor () {
+        this.root = null
+    }
+    // various search functions
+    searchByName () {
+
+    }
+
+    searchByNameR () {
+        
+    }
+}
