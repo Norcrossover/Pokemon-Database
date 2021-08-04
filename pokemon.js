a2241a5d28cb6e79bca2ec1c6b5ac74ece07811a
Kobe <kobenorcross@gmail.com>
2021-08-04 15:25:08 -0700

    Added async function to receive my pokemon.csv

--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,9 @@
-`
+
 import * as data from 'pokemon.json';
 const pokemon = data;
 console.log(JSON.parse(pokemon))
 
-
+`
 let pokemon;    
 fetch('pokemon.json').then(
         function(u){ return u.json();}
@@ -12,10 +12,10 @@ fetch('pokemon.json').then(
           pokemon = json;
         }
       )
+`
 
-
-async function getData(url) {
-    const response = await fetch(url);
+async function getData() {
+    const response = await fetch('pokemon.csv');
 
     return response.json();
 }
@@ -23,7 +23,7 @@ async function getData(url) {
 const pokemon = getData('pokemon.json');
 
 console.log({ pokemon })
-`
+
 
 
 const file = '/Users/kobenorcross/Documents/GitHub/Pokemon-Database'
@@ -45,6 +45,13 @@ function readTextFile(file)
     }
     rawFile.send(null);
 }
+
+/*
+// asynch wait function to read in CSV file possibility
+async readCSV() {
+    
+}
+*/
 
 class Pokemon {
     // NUMBER,CODE,SERIAL,NAME,TYPE1,TYPE2,COLOR,ABILITY1,ABILITY2,ABILITY HIDDEN,GENERATION,LEGENDARY,MEGA_EVOLUTION,HEIGHT,WEIGHT,HP,ATK,DEF,SP_ATK,SP_DEF,SPD,TOTAL
